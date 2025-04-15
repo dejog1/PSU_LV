76acfea92b7a9f9f257319e3f28de5503ab4f408
dejog1 <dejangrabovac1@gmail.com>
2025-04-15 11:41:06 +0200

    Create 1.zad.py

---
+++ LV1/1.zad.py
@@ -0,0 +1,18 @@
+# 1. Napišite program koji od korisnika zahtijeva unos radnih sati te koliko je plaćen po radnom satu. Koristite ugrađenu
+# Python metodu input(). Nakon toga izračunajte koliko je korisnik zaradio i ispišite na ekran. Na kraju prepravite
+# rješenje na način da ukupni iznos izračunavate u zasebnoj funkciji naziva total_euro.
+
+
+# def total_euro(brojSati, satnica):
+#     return brojSati * satnica
+
+
+# 35
+# brojSati = float(input("Unesite broj odradjenih sati:"))
+# satnica = float(input("Unesite vasu satnicu:"))
+
+
+
+# total= total_euro(brojSati, satnica)
+
+# print(f"Zarada: {total} eura")
